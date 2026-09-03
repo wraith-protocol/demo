ec446ee13e46b09028b109f5904199e0b9ce6653
truthixify <truthixify@gmail.com>
2026-09-03 21:45:56 +0100

    style: format batch template files with prettier

--- src/main.tsx
+++ src/main.tsx
@@ -77,7 +77,7 @@ function Providers({ children }: { children: React.ReactNode }) {
                       <StellarWalletProvider>
                         <ContactsProvider>
                           <NameHistoryProvider>
-                          <SplitTemplatesProvider>
+                            <SplitTemplatesProvider>
                               <StealthKeysProvider>
                                 <ScanStrategyProvider>{children}</ScanStrategyProvider>
                               </StealthKeysProvider>
